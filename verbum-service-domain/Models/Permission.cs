a86d27d82e4f57515b86521788f2ac3d76cd80f6
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-09-16 22:25:35 +0700

    fix data model

--- verbum-service-domain/Models/Permission.cs
+++ verbum-service-domain/Models/Permission.cs
@@ -7,7 +7,7 @@ namespace verbum_service_domain.Models
     {
         public Permission()
         {
-            UserPermissions = new HashSet<UserPermission>();
+            UserCompanies = new HashSet<UserCompany>();
         }
 
         public int Id { get; set; }
@@ -15,6 +15,6 @@ namespace verbum_service_domain.Models
         public string Action { get; set; } = null!;
         public string Entity { get; set; } = null!;
 
-        public virtual ICollection<UserPermission> UserPermissions { get; set; }
+        public virtual ICollection<UserCompany> UserCompanies { get; set; }
     }
 }
